54684ec686861cb5e1ef71200cdf933b9590f25b
K M Marziul Karim <marziulrafi@gmail.com>
2025-02-05 00:39:07 +0600

    tasks

---
+++ task01.js
@@ -0,0 +1,4 @@
+var money = 1000;
+var cost = 700;
+var returnn = money - cost
+console.log(returnn)
---
+++ task02.js
@@ -0,0 +1,11 @@
+var math = 75.25
+var biology = 65
+var chemistry = 80
+var physics = 35.45
+var bangla = 99.50
+
+var total = math+biology+chemistry+physics+bangla
+var subjects = 5
+
+var average = total/subjects
+console.log(average.toFixed(2))
